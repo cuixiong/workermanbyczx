f632c7e81c80b4067810cb6cb27ed25566891c7f
czx <798396652@qq.com>
2019-01-05 17:39:19 +0800

    回调地址

---
+++ thinkphp_5.0.22/application/index/controller/Callback.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/1/5
+ * Time: 16:34
+ */
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Callback extends Controller
+{
+    public function qq_callback()
+    {
+        return "200";
+    }
+}
